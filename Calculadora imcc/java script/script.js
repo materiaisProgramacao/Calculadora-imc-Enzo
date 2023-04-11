80ed15e38236f361615b3e4870aa805422290893
materiaisProgramacao <108790443+materiaisProgramacao@users.noreply.github.com>
2023-04-11 15:52:22 -0300

    Add files via upload

--- Calculadora imcc/java script/script.js
+++ Calculadora imcc/java script/script.js
@@ -1,3 +1,13 @@
 function dividir() {
-    spanID.innerHTML = Number(peso.value) / Number(altura.value)
+   
+    let span = document.getElementById("spanId");
+
+    let peso = document.getElementById("peso").value;
+
+    let altura = document.getElementById("altura").value;
+
+    resultado = peso/altura** 2;
+
+ span.textContent = Math.round(resultado) 
+
 }
